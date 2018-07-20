be9b562287a354bd61e62496a5ed6fccab63b1b0
Mohammed Shakir M K <mmanamku@allgosystems.com>
2018-07-20 12:05:32 +0530

    Similarity Tester Code Added
    -- sim_pearson
    -- sim_distance

--- test_code.py
+++ test_code.py
@@ -1,2 +1,3 @@
 import recommendations
-print recommendations.sim_distance(recommendations.critics, 'Lisa Rose', 'Gene Seymour')
+#print recommendations.sim_distance(recommendations.critics, 'Lisa Rose', 'Gene Seymour')
+print recommendations.topMatches(recommendations.critics, 'Toby', n=3)
